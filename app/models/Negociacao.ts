475d941248932f55419635c35e415d8fe3292dab
William Rodrigues <williamrodrigues2442@gmail.com>
2021-12-19 17:00:49 -0300

    Um pouco sobre programaçao defensiva

--- app/models/Negociacao.ts
+++ app/models/Negociacao.ts
@@ -1,9 +1,14 @@
 export class Negociacao {
   constructor(
-    public readonly data: Date,
+    private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
   ) {}
+
+  get data(): Date {
+    const data = new Date(this._data.getTime());
+    return data;
+  }
 
   get volume(): number {
     return this.quantidade * this.valor;
